b963948b02b95dea104e9ddc6f840815de1ea72e
Mohamed <mohameddrame907@gmail.com>
2022-11-16 23:24:18 +0100

    add some coments

--- lib/services/login_service.dart
+++ lib/services/login_service.dart
@@ -6,12 +6,10 @@ import 'mongodb.dart';
 
 getUser(String username, String mdp) async {
   var db = await MongoDataBase.connect();
-  var user = await db.collection(COLLECTION_NAME).find({
+  var user = await db.collection(COLLECTION_NAME).find({  //  Recherche de l'utilisteur dans la db en fonction de son username et mot de passe
     'username': username,
     'userMdp': mdp
-  }).toList();
+  }).toList(); // Mettre ensuite le JSON de l'utilisateur dans un tableau
 
-  print("**************");
   print(user);
-  print("**************");
 }
