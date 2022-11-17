6a8341b66748132173a187e3ccadc71393020c9d
Julien ROBIN <julien.jean.robin@gmail.com>
2022-11-17 23:35:47 +0100

    affichage des types d'actualités

--- lib/controller/actualites_controller.dart
+++ lib/controller/actualites_controller.dart
@@ -3,10 +3,10 @@ import 'package:gestion_ecurie/services/mongodb.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
 class ActualitesController {
-  static insert() async {
+  static void insert() async {
     //ajoute un évènement correspondant (pour l'instant) à l'inscription du premier utilisateur trouvé
     var db = await MongoDataBase.connect();
-    DbCollection newsCollection = await db.collection('Actualites');
+    DbCollection newsCollection = db.collection('Actualites');
     DbCollection usersCollection = db.collection('Users');
 
     var author = await usersCollection.find().first;
@@ -14,5 +14,18 @@ class ActualitesController {
 
     Actualite news = Actualite("Inscription", author['_id'], DateTime.now(), DateTime.now());
     newsCollection.insertOne(news.toMap());
+  }
+
+  static Future<List<Actualite>> fetchNews() async {
+    List<Actualite> newsList = [];
+    var db = await MongoDataBase.connect();
+    DbCollection newsCollection = await db.collection('Actualites');
+    var dbNews = await newsCollection.find();
+    dbNews.forEach((news) {
+      Actualite actualite = Actualite(news['eventType'], news['_author'], news['endDate'], news['creationDate']);
+      print(actualite);
+      newsList.add(actualite);
+    });
+    return newsList;
   }
 }
--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gestion_ecurie/controller/actualites_controller.dart';
+import 'package:gestion_ecurie/models/actualite.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -11,41 +12,97 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
+  List<Actualite> news = [];
+  
+  clearLocalStorage() {
+    setState(() {
+      LocalStorageHelper.clearAll();
+    });
+  }
 
-  void _newEvent() {
-    ActualitesController.insert();
-    // ajoute une fausse inscription aux actualités
+  void refreshNews() async{
+    var dbNews = await ActualitesController.fetchNews();
+    setState(() {
+      news = dbNews;
+    });
+  }
+
+  Card _inscriptionNewsCard(Actualite news){
+    return Card(
+      child: Row(
+        children: [
+          Icon(Icons.newspaper),
+          Text(news.eventType),
+        ],
+      )
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    //refreshNews();
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.person_add), onPressed: () {}),
-          IconButton(icon: Icon(Icons.login), onPressed: () {})
+          IconButton(
+              icon: Icon(Icons.home),
+              onPressed :() => context.go('/')),
+
+          IconButton(
+              icon: Icon(Icons.person_add),
+              onPressed: () {
+                // Ici je créer la popup qui affichera le formulaire de création de compte
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        content: Stack(
+                          clipBehavior: Clip.none, children: <Widget>[
+                          SignupPop()
+                        ],
+                        ),
+                      );
+                    });
+              }),
+          IconButton(
+            icon: Icon(Icons.login),
+            onPressed: () => showDialog<String>(
+              context: context,
+              builder: (BuildContext context) => AlertDialog(
+                // déclaration de la pop up
+                title: Center(child: const Text('Connexion')),
+                actions: <Widget>[
+                  FormLogin()
+                ],
+              ),
+            ),
+          )
         ],
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have created this many events:',
-            ),
             Text(
-              '$_counter',
+              'Actualités du club',
               style: Theme.of(context).textTheme.headline4,
+            ),
+            ListView.builder(
+              itemCount: news.length,
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+                itemBuilder: (BuildContext context, int index) {
+                  return _inscriptionNewsCard(news[index]);
+                }
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _newEvent,
-        tooltip: 'new event',
-        child: const Icon(Icons.add),
+        onPressed: refreshNews,
+        tooltip: 'refresh',
+        child: const Icon(Icons.refresh),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
